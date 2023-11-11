c1c1d6f35ffb78a0e9970b386cd9358a13e20352
Leonardo Massena <leonardomassena19@gmail.com>
2023-11-11 08:51:43 -0300

    absolute url

--- DilithiumClient/src/App.js
+++ DilithiumClient/src/App.js
@@ -5,35 +5,37 @@ import { CertiCreate } from './CertiCreate'
 import { CertiAdmin } from './CertiAdmin'
 import { CertiHome } from './CertiHome'
 import { CertiSign } from './CertiSign'
+import { CertiShow } from './CertiShow'
 import { Navbar, Nav, Container, Carousel } from 'react-bootstrap'
 import { HOST, PORT } from './constant'
 const App = () => {
- 
+
   return (
 
     <Router>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="home">Página inicial</Navbar.Brand>
+          <Navbar.Brand href="/home">Página inicial</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="search">Procurar diploma por id</Nav.Link>
-              <Nav.Link href="create">Criar diploma</Nav.Link>
-              <Nav.Link href="admin">Gerenciar diplomas</Nav.Link>
-              <Nav.Link href="sign">Assinar documento</Nav.Link>
+              <Nav.Link href="/search">Procurar diploma por id</Nav.Link>
+              <Nav.Link href="/create">Criar diploma</Nav.Link>
+              <Nav.Link href="/admin">Gerenciar diplomas</Nav.Link>
+              <Nav.Link href="/sign">Assinar documento</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <Routes>
 
-        <Route exact path="/" element={<CertiHome />} />
-        <Route exact path="/home" element={<CertiHome />} />
-        <Route exact path="/search" element={<CertiSearch />} />
-        <Route exact path="/create" element={<CertiCreate />} />
-        <Route exact path="/admin" element={<CertiAdmin />} />
-        <Route exact path="/sign" element={<CertiSign />} />
+        <Route path="/" element={<CertiHome />} />
+        <Route path="/home" element={<CertiHome />} />
+        <Route path="/search" element={<CertiSearch />} />
+        <Route path="/create" element={<CertiCreate />} />
+        <Route path="/admin" element={<CertiAdmin />} />
+        <Route path="/sign" element={<CertiSign />} />
+        <Route path="/certificate/:id" element={<CertiShow />} />
       </Routes>
       <div className="App"></div>
     </Router >
